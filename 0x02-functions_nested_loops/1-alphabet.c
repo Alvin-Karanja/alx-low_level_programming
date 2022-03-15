238febadd7f2045f7104f85318a31a9e4ac129f5
Alvin-Karanja <alvin2@protonmail.com>
2022-03-15 14:07:06 +0300

    Add files

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,11 +6,14 @@
  */
 void print_alphabet(void)
 {
-    char c;
+    char c = 'a';
 
-    for (c = 'a'; c <= 'z'; c++)
+    while (c <= 'z')
     {
+        /* code */
         _putchar(c);
+        c++;
     }
     _putchar('\n');
+    
 }
