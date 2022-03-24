2d7dabf55123b6805de14066507ca7aad42052db
Alvin-Karanja <alvin2@protonmail.com>
2022-03-24 11:04:17 +0300

    refactor

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,3 +1,5 @@
+
+  
 #include "main.h"
 /**
  * _strcat - appends src to the dest string
@@ -8,14 +10,14 @@
  */
 char *_strcat(char *dest, char *src)
 {
-    int i, j;
-    i = j = 0;
-    while (*(dest + i))
-        i++;
-    while ((*(dest + i) = *(src + j)))
-    {
-        i++;
-        j++;
-    }
-    return (dest);
+int i, j;
+i = j = 0;
+while (*(dest + i))
+i++;
+while ((*(dest + i) = *(src + j)))
+{
+i++;
+j++;
+}
+return (dest);
 }
