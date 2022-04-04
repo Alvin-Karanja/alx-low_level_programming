4c8efd11a85489b1b0ba8a87a44b74b573532acd
Alvin-Karanja <alvin2@protonmail.com>
2022-04-04 13:24:51 +0300

    My commit on 0x0B-malloc_free project

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _strdup - allocates space in memory
+ * @str: input string
+ *
+ * Return: returns a pointer to a newly
+ * allocated space in memory
+ */
+
+char *_strdup(char *str)
+{
+char *duplicate;
+int index, len = 0;
+
+if (str == NULL)
+return (NULL);
+
+for (index = 0; str[index]; index++)
+len++;
+
+duplicate = malloc(sizeof(char) * (len + 1));
+
+if (duplicate == NULL)
+return (NULL);
+
+for (index = 0; str[index]; index++)
+duplicate[index] = str[index];
+
+duplicate[len] = '\0';
+
+return (duplicate);
+}
